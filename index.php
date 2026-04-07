af6ea1e899c74fbdb3babf86fac1927b061e9a56
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2026-04-07 14:55:04 +0200

    Exercice 2

--- index.php
+++ index.php
@@ -1,11 +1,19 @@
 <?php
 
 require __DIR__.'/EventDispatcher.php';
+require __DIR__.'/EventListenerInterface.php';
 
+$listener = new class implements EventListenerInterface {
+    public function handle(object $event): void
+    {
+        echo 'Handled event: '.$event->getMessage()."\n";
+    }
+};
 $dispatcher = new EventDispatcher();
 $dispatcher->addListener('foo', function ($event) {
     echo 'Foo event: '.$event->getMessage()."\n";
 });
+$dispatcher->addListener('foo', $listener);
 
 $dispatcher->dispatch(new class {
     public function getMessage(): string
@@ -13,3 +21,4 @@ $dispatcher->dispatch(new class {
         return 'Hello World!';
     }
 }, 'foo');
+
