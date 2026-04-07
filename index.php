13fdabe1389457e99a2db979a50973a2a5b677a5
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2026-04-07 17:06:24 +0200

    Add Twig

--- index.php
+++ index.php
@@ -2,13 +2,21 @@
 
 use App\EventDispatcher;
 use App\Interface\EventListenerInterface;
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
 
 require_once __DIR__.'/vendor/autoload.php';
 
-$listener = new class implements EventListenerInterface {
+$twig = new Environment(new FilesystemLoader('templates'));
+
+$listener = new class($twig) implements EventListenerInterface {
+    public function __construct(
+        private readonly Environment $twig,
+    ) {}
+
     public function handle(object $event): void
     {
-        echo 'Handled event: '.$event->getMessage()."\n";
+        echo $this->twig->render('event.html.twig', ['message' => $event->getMessage()]);
     }
 };
 $dispatcher = new EventDispatcher();
