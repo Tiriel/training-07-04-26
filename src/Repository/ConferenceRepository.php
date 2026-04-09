024fa531d921fd11141e79b3df6363a7e8f16cde
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2026-04-09 11:28:13 +0200

    Exercise 15

--- src/Repository/ConferenceRepository.php
+++ src/Repository/ConferenceRepository.php
@@ -16,6 +16,47 @@ class ConferenceRepository extends ServiceEntityRepository
         parent::__construct($registry, Conference::class);
     }
 
+    public function findBetweenDates(?\DateTimeImmutable $startAt = null, ?\DateTimeImmutable $endAt = null): array
+    {
+        if (null === $startAt && null === $endAt) {
+            throw new \InvalidArgumentException('At least one of the dates must be provided.');
+        }
+
+        $qb = $this->createQueryBuilder('c');
+
+        if ($startAt instanceof \DateTimeImmutable) {
+            $qb->andWhere($qb->expr()->gte('c.startAt', ':startAt'))
+               ->setParameter('startAt', $startAt);
+        }
+
+        if ($endAt instanceof \DateTimeImmutable) {
+            $qb->andWhere($qb->expr()->lte('c.endAt', ':endAt'))
+               ->setParameter('endAt', $endAt);
+        }
+
+        return $qb
+            ->getQuery()
+            ->getResult();
+    }
+
+    public function save(Conference $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function remove(Conference $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
     //    /**
     //     * @return Conference[] Returns an array of Conference objects
     //     */
