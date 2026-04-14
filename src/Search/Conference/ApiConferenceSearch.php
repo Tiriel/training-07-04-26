11fad870e91469c878f5c1f9c878d840cc4e37a7
Adrien Roches <adrien@roc-it.tech>
2026-04-14 14:55:23 +0200

    [sf-advanced] Use Api class and consume remote API (not stable)

--- src/Search/Conference/ApiConferenceSearch.php
+++ src/Search/Conference/ApiConferenceSearch.php
@@ -6,6 +6,7 @@ namespace App\Search\Conference;
 
 use Symfony\Component\DependencyInjection\Attribute\AsAlias;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 #[AsAlias(id: ConferenceSearchInterface::class)]
 final class ApiConferenceSearch implements ConferenceSearchInterface
@@ -13,11 +14,35 @@ final class ApiConferenceSearch implements ConferenceSearchInterface
     public function __construct(
         #[Autowire(env: 'CONFERENCES_API_KEY')]
         private readonly string $apiKey,
+
+        private readonly HttpClientInterface $httpClient,
     ) {
     }
 
-    public function searchByName(?string $name = null): array
+    public function searchByName(string|null $name = null): array
     {
+        $httpClient = $this->httpClient
+            ->withOptions([
+                'headers' => [
+                    'apikey' => $this->apiKey,
+                    'accept' => 'application/json'
+                ],
+            ])
+        ;
+
+        $name = trim($name ?? '');
+
+        if ('' !== $name) {
+            $httpClient = $httpClient
+                ->withOptions([
+                    'query' => ['name' => $name],
+                ])
+            ;
+        }
+
+        $response = $httpClient->request('GET', 'https://devevents-api.fr/events');
+        dump($response->toArray());
+
         return [];
     }
 }
