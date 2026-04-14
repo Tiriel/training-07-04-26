347a901563521af002ba5cc7d50b794b569817c4
Adrien Roches <adrien@roc-it.tech>
2026-04-14 15:10:22 +0200

    [sf-advanced] Define and use custom http client for devevents

--- src/Search/Conference/ApiConferenceSearch.php
+++ src/Search/Conference/ApiConferenceSearch.php
@@ -5,42 +5,30 @@ declare(strict_types=1);
 namespace App\Search\Conference;
 
 use Symfony\Component\DependencyInjection\Attribute\AsAlias;
-use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Component\DependencyInjection\Attribute\Target;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 #[AsAlias(id: ConferenceSearchInterface::class)]
 final class ApiConferenceSearch implements ConferenceSearchInterface
 {
     public function __construct(
-        #[Autowire(env: 'CONFERENCES_API_KEY')]
-        private readonly string $apiKey,
-
+        #[Target('conferences.client')]
         private readonly HttpClientInterface $httpClient,
     ) {
     }
 
     public function searchByName(string|null $name = null): array
     {
-        $httpClient = $this->httpClient
-            ->withOptions([
-                'headers' => [
-                    'apikey' => $this->apiKey,
-                    'accept' => 'application/json'
-                ],
-            ])
-        ;
+        $options = [];
 
         $name = trim($name ?? '');
 
         if ('' !== $name) {
-            $httpClient = $httpClient
-                ->withOptions([
-                    'query' => ['name' => $name],
-                ])
-            ;
+            $options['query'] ??= [];
+            $options['query']['name'] = $name;
         }
 
-        $response = $httpClient->request('GET', 'https://devevents-api.fr/events');
+        $response = $this->httpClient->request('GET', '/events', $options);
         dump($response->toArray());
 
         return [];
