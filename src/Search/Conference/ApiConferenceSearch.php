fd9820530377391160795d063c84e8fa169b246a
Adrien Roches <adrien@roc-it.tech>
2026-04-14 14:12:46 +0200

    [sf-advanced] Use alias on DatabaseConferenceSearch

--- src/Search/Conference/ApiConferenceSearch.php
+++ src/Search/Conference/ApiConferenceSearch.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace App\Search\Conference;
 
+use Symfony\Component\DependencyInjection\Attribute\AsAlias;
+
+#[AsAlias(id: ConferenceSearchInterface::class)]
 final class ApiConferenceSearch implements ConferenceSearchInterface
 {
     public function searchByName(?string $name = null): array
